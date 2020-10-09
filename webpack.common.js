9fe866f2473e8b8a903226db92431efb312c942c
Roman Revunenkov <romik.rev@gmail.com>
2020-10-09 12:30:56 +0300

    bugfix image optimization

--- webpack.common.js
+++ webpack.common.js
@@ -46,23 +46,23 @@ module.exports = {
           {
             loader: "image-webpack-loader",
             options: {
-              // mozjpeg: {
-              //   progressive: true,
-              //   quality: 65
-              // },
-              // optipng: {
-              //   enabled: false,
-              // },
-              // pngquant: {
-              //   quality: '65-90',
-              //   speed: 4
-              // },
-              // gifsicle: {
-              //   interlaced: false,
-              // },
-              // svgo: {
-              //   enabled: false,
-              // }
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              svgo: {
+                enabled: false,
+              }
             },
           },
         ],
